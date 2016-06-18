f87be26771ac68a3ec15ffe69fd5eebb23c94c16
Jose Ignacio Gonzalez Cardenas <nacho.gon.car@gmail.com>
2016-06-18 16:54:06 +0200

    Testing with multiple spheres.

--- objectsDefinition.js
+++ objectsDefinition.js
@@ -11,7 +11,7 @@ function addObjects() {
     sphere.color = [0.0, 0.5, 0.75, 0.75];
     objects.push(sphere);
     
-    /*
+    
     sphere = new Sphere();
     sphere.position = [1.0, 1.0, 0.0, 1.0];
     sphere.color = [1.0, 0.3, 0.5, 0.75];
@@ -19,7 +19,7 @@ function addObjects() {
     
     
     sphere = new Sphere();
-    sphere.position = [1.0, 1.0, 0.0, 1.0];
+    sphere.position = [1.0, 3.0, 0.0, 1.0];
     sphere.color = [1.0, 1.0, 0.0, 0.75];
     objects.push(sphere);
     
@@ -32,7 +32,7 @@ function addObjects() {
     sphere.position = [1.0, 1.0, 0.0, 1.0];
     sphere.color = [0.0, 0.0, 0.0, 1.0];
     objects.push(sphere);
-    */
+    
     ///
     
     return objects;
